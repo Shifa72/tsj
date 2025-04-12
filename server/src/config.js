d5b7c099a9ae634df5873ab7b2420478bc5b292f
IS <shagbanov72@mail.ru>
2025-04-12 22:41:49 +0500

    Updates

--- server/src/config.js
+++
@@ -1,5 +0,0 @@
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
-
-module.exports = {
-  JWT_SECRET
-}; 
--- src/pages/Conference.tsx
+++
@@ -1,9 +0,0 @@
-
-const Conference = () => {
-    return (
-        <div>
-            <h1>Conference</h1>
-        </div>
-    )
-}
-export default Conference
